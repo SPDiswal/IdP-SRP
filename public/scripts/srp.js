b32ff22bfa5a3ddf913b891dc6d427e42a3dd1ba
Steffen Diswal <steffen.diswal@gmail.com>
2015-08-10 13:38:21 +0200

    #bugfix

--- public/scripts/srp.js
+++ public/scripts/srp.js
@@ -10,7 +10,7 @@ function signIn()
 {
     $("#error").hide();
     $("#success").hide();
-    $("#message").text();
+    $("#message").text("");
 
     clientHello()
         .then(clientKeyExchange)
